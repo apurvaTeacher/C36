25d5b8f7ac1ca8c29156c2531b60f3250265246e
apurvaTeacher <76532533+apurvaTeacher@users.noreply.github.com>
2021-07-25 17:33:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
+
